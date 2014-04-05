8a073eb425a7f3529e79926554d16127c4d59fe0
Abigail Watson <awatson1978@gmail.com>
2014-04-05 13:07:29 -0400

    updated browser policy

--- server/browser.policy.js
+++ server/browser.policy.js
@@ -4,5 +4,5 @@ BrowserPolicy.content.allowSameOriginForAll();
 BrowserPolicy.content.allowDataUrlForAll("http://localhost:3200");
 BrowserPolicy.content.allowOriginForAll("http://localhost:3200");
 
-BrowserPolicy.content.allowDataUrlForAll("http://panel-layout-testrunner.meteor.com");
-BrowserPolicy.content.allowOriginForAll("http://panel-layout-testrunner.meteor.com");
+BrowserPolicy.content.allowDataUrlForAll("http://panel-layout-testrunner.meteor.com/");
+BrowserPolicy.content.allowOriginForAll("http://panel-layout-testrunner.meteor.com/");
