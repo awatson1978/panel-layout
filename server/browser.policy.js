6fb4b49567322779fe3bb151790e3bcdf68ef5a2
Abigail Watson <awatson1978@gmail.com>
2015-02-01 16:20:24 -0500

    upgrade to meteor 1.0, updated packages, brower-policy removed, nightwatch, travis, etc

--- server/browser.policy.js
+++
@@ -1,8 +0,0 @@
-BrowserPolicy.framing.allowAll();
-
-BrowserPolicy.content.allowSameOriginForAll();
-BrowserPolicy.content.allowDataUrlForAll("http://localhost:3200");
-BrowserPolicy.content.allowOriginForAll("http://localhost:3200");
-
-BrowserPolicy.content.allowDataUrlForAll("http://panel-layout-testrunner.meteor.com/");
-BrowserPolicy.content.allowOriginForAll("http://panel-layout-testrunner.meteor.com/");
---
+++ webapp/tests/nightwatch/appLayout.walkthrough.js
@@ -0,0 +1,12 @@
+// tests/leaderboard.js
+module.exports = {
+  "ActiveRecord::Table Walkthrough" : function (client) {
+    client
+    .url("http://localhost:3000")
+    .waitForElementVisible('body', 1000)
+    .pause(2000)
+
+
+    .end();
+  }
+}
